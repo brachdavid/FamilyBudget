6788cff0fdd665503d46aa4bfd766e70e6267547
brachdavid <153853659+brachdavid@users.noreply.github.com>
2024-06-15 21:37:51 +0200

    Add FamilyBudget application

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FamilyBudget
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
